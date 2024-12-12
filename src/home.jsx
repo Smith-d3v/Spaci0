1676c76196b81728081b6c3a69442a36762808d3
Smith-d3v <smithuzoagba@gmail.com>
2024-12-12 05:55:53 -0800

    changes

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 import { SignInButton } from '@clerk/clerk-react'
 import React from 'react'
 import { Button } from './components/ui/button'
-import Header from './components/header'
+import Header from './components/Header'
 import Hero from './components/Hero'
 import Category from './components/Category'
 import MostSearchedProperty from './components/MostSearchedProperty'
